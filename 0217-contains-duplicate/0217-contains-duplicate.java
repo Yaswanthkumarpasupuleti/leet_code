ec1609eb8dc254d7e6d916cb82434f09d9d2bf7c
Yaswanth Kumar Pasupuleti <yaswanthkumarpasupulati@gmail.com>
2025-08-30 10:45:49 +0530

    Time: 19 ms (36.75%), Space: 61.2 MB (7.21%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i = 0; i < n;i++){
+            hs.add(nums[i]);
+        }
+        return hs.size() != n;
+    }
+}
